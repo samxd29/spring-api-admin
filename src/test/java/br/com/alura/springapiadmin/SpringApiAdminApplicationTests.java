15d79504cca49db0e5e2988e0566532585e6ae87
samxd29 <carvalholuzs@gmail.com>
2022-11-11 18:53:14 -0300

    Initial commit

---
+++ src/test/java/br/com/alura/springapiadmin/SpringApiAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.springapiadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
